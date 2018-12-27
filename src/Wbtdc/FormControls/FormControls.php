52abad168ae7216cfe5bdfa9121701396027b3ac
davetbo <davetbo@192.168.10.123>
2018-12-27 14:29:41 -0800

    display_switch working.

--- src/Wbtdc/FormControls/FormControls.php
+++ src/Wbtdc/FormControls/FormControls.php
@@ -17,11 +17,11 @@ class FormControls {
         }
     }
     protected function printStyles() { ?>
+    	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->    
     	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <?php     
+		<?php     
     }
     public function display_switch($args) {
         $this->checkPrintedStyles();
@@ -33,13 +33,10 @@ class FormControls {
         $validation = array_key_exists('validation', $args) ? $args['validation'] : '';
 
         $checked = $value == 'on' ? ' checked' : '';?>
-		<input id="<?php echo $name;?>" name="<?php echo $name;?>" type="checkbox" data-on-text="<?php echo $args['on-text'];?>" data-on-color="info" data-off-color="warning" data-off-text="<?php echo $args['off-text'];?>" <?php echo $checked;?>>
-        
-        <script type="text/javascript">
-        jQuery(document).ready(function() {
-            jQuery('#<?php echo $name;?>').bootstrapSwitch();
-        });
-        </script>
+        <div class="custom-control custom-switch">
+  			<input type="checkbox" class="custom-control-input <?php echo $args['class'];?>" id="<?php echo $name;?>" name="<?php echo $name;?>"  <?php echo $checked;?>/>
+  			<label class="custom-control-label" for="<?php echo $name;?>"><?php echo $args['label-text'];?></label>
+		</div>
     <?php 
     }
     public function display_colorpicker($args) {
